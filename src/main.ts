7ea6d91bfdb9d9b365ad4bab9dde9c9bfb8bc946
Meet Makwana <meetmakwana203@gmail.com>
2025-11-12 20:53:47 +0530

    fix: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOrigins = process.env.CORS_ORIGINS?.split(',') || [];
+
+  app.enableCors({
+    origin: corsOrigins,
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => console.error(err));
